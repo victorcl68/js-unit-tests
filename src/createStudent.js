206fa30880157a6a9778ce1f5e3110fc233a8669
Victor Cabrera Lopes Cardoso <victor.cabrera0209@gmail.com>
2021-03-27 17:49:51 -0300

    Quinto requisito resolucao

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,9 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
+const createStudent = (theName) => ({
+  name: theName,
+  feedback: () => 'Eita pessoa boa!',
+});
 
 module.exports = createStudent;
