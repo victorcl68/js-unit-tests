553f5742398e8060456dfe842d697e08ab54b3ec
Victor Cabrera Lopes Cardoso <victor.cabrera0209@gmail.com>
2021-03-27 18:24:26 -0300

    Sexto requisito resolucao

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -33,12 +33,19 @@ const productDetails = require('../src/productDetails');
 
 describe('#productDetails', () => {
   it('tests the function has the correct behaviour', () => {
-    assert.fail();
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste que o retorno da função é um array.
+    assert.strictEqual(Array.isArray(productDetails(1, 2)), true);
     // Teste que o array retornado pela função contém dois itens dentro.
+    assert.strictEqual(productDetails(0, 1).length, 2);
     // Teste que os dois itens dentro do array retornado pela função são objetos.
+    assert.strictEqual(typeof productDetails()[0], 'object');
+    assert.strictEqual(typeof productDetails()[1], 'object');
     // Teste que os dois objetos são diferentes entre si.
+    assert.strictEqual(productDetails()[0] !== productDetails()[1], true);
     // (Difícil) Teste que os dois productIds terminam com 123.
+    assert.strictEqual(productDetails()[0].details.productId.endsWith('123'), true);
+    assert.strictEqual(productDetails()[1].details.productId.endsWith('123'), true);
+    // Após muitas tentativas e pesquisar dei uma olhada no modo como meus colegas resolveram esse último teste e me deparei com o método endsWith, no Github do Emerson Saturnino!
   });
 });
