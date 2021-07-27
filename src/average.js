61decc9dc08e73fdb67646d74b3fcc8a6e54634c
Victor Lopes <victor.cabrera0209@gmail.com>
2021-07-26 21:32:27 -0300

    avarage function reset

--- src/average.js
+++ src/average.js
@@ -12,18 +12,6 @@
     - average([1, '2']) // Retorno: undefined;
 */
 
-const average = (array) => {
-  let soma = 0;
-  if (array.length === 0) {
-    return undefined;
-  }
-  for (let index = 0; index < array.length; index += 1) {
-    if (typeof (array[index]) !== 'number') {
-      return undefined;
-    }
-    soma += array[index];
-  }
-  return Math.round(soma / (array.length));
-};
-// Não conseguia resolver a função mas ao ver a resolução no Github do Thiago Felipe, consegui corrigir os pequenos erros do meu código;
+const average = () => {};
+
 module.exports = average;
