e3f14753f8c64b0f37fb5325c297202680aa3676
Victor Lopes <victor.cabrera0209@gmail.com>
2021-09-22 09:37:46 -0300

    Trying another time solve github evaluator problem

--- src/average.js
+++ src/average.js
@@ -3,7 +3,6 @@
   Caso a função receba algum valor não númerico ou um array vazio,
   o valor undefined deve ser retornado.
   Todos os resultados devem ser arredondados para valores inteiros. Ex: 4,6 vira 5; 1,3 vira 1.
-
   Parâmetros:
     - Um array. Exemplos: [1, 2]; [1, 2, 3, 4, 5]; [1, 2, '3']; [];
   Comportamento:
@@ -11,7 +10,6 @@
     - average([1, 1]) // Retorno: 1;
     - average([1, '2']) // Retorno: undefined;
 */
-
 const average = (array) => {
   let soma = 0;
   if (array.length === 0) {
