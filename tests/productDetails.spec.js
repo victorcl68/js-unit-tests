64cb767201c953d4e2a836a7a22f831469edee8b
Victor Lopes <victor.cabrera0209@gmail.com>
2021-10-09 14:05:36 -0300

    Taking off unnecessary comments

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -4,48 +4,14 @@
 const assert = require('assert');
 const productDetails = require('../src/productDetails');
 
-/*
-  Dadas duas strings que representam nomes de produtos, retorne um array contendo dois objetos com os detalhes dos respectivos produtos.
-
-  Parâmetros:
-  - Uma string;
-  - Uma string;
-
-  Comportamento:
-  productDetails('Alcool gel', 'Máscara') // Retorna:
-  [
-    {
-      name: 'Alcool gel'
-      details: {
-        productId: 'Alcool gel123'
-      }
-    },
-    {
-      name: 'Máscara'
-      details: {
-        productId: 'Máscara123'
-      }
-    }
-  ]
-
-  OBS: Lembre-se que você não precisa se preocupar com o describe e o it por enquanto, isso será aprendido posteriormente.
-*/
-
 describe('#productDetails', () => {
   it('tests the function has the correct behaviour', () => {
-    // ESCREVA SEUS TESTES ABAIXO:
-    // Teste que o retorno da função é um array.
     assert.strictEqual(Array.isArray(productDetails(1, 2)), true);
-    // Teste que o array retornado pela função contém dois itens dentro.
     assert.strictEqual(productDetails(0, 1).length, 2);
-    // Teste que os dois itens dentro do array retornado pela função são objetos.
     assert.strictEqual(typeof productDetails()[0], 'object');
     assert.strictEqual(typeof productDetails()[1], 'object');
-    // Teste que os dois objetos são diferentes entre si.
     assert.strictEqual(productDetails()[0] !== productDetails()[1], true);
-    // (Difícil) Teste que os dois productIds terminam com 123.
     assert.strictEqual(productDetails()[0].details.productId.endsWith('123'), true);
     assert.strictEqual(productDetails()[1].details.productId.endsWith('123'), true);
-    // Após muitas tentativas e pesquisar dei uma olhada no modo como meus colegas resolveram esse último teste e me deparei com o método endsWith, no Github do Emerson Saturnino!
   });
 });
